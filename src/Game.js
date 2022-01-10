ad1503fc8b3d91292d6fe7aef78999d3f8a05793
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2022-01-11 07:30:36 +0800

    feat: Create game loop

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,34 @@
 const readline = require('readline');
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
+const msg_welcome = "Welcome!";
+const msg_goodbye = "Goodbye!";
+
+const prompt_continue = "Press <ENTER> to continue...";
+const prompt_chooseOption = "What would you like to do?";
+
+function deepcopy(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+function equals(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
+}
+
+function formatWithPadding(message) {
+  return `\n${message}\n`;
+}
+
+function formatAsMenu(options, header = prompt_chooseOption) {
+  let menu = header;
+
+  for (const [i, option] of Object.entries(options)) {
+    menu += `\n[${parseInt(i) + 1}] ${option.summary}`;
+  };
+
+  return menu;
+}
+
 function ask(query = "") {
   return new Promise((resolve, reject) => {
     readlineInterface.question(query, resolve);
@@ -19,55 +47,158 @@ function displayLines(messages) {
   console.log(messages.join('\n'));
 }
 
-function formatPrompt(prompt) {
-  return `\n${prompt}\n`;
+function displayDebug(state, room) {
+  console.log(state);
+  console.log(room.options);
 }
-
-function formatOption(option, i) {
-  return `[${i}] ${option}`;
-}
-
-const msg_welcome = "Welcome!";
-const msg_goodbye = "Goodbye!";
-
-const prompt_continue = "Press <ENTER> to continue...";
-const prompt_chooseOption = "What would you like to do?";
-
-const desc_room = [
-  "You are trapped...",
-  "",
-  "On the left, you see an sink.",
-  "Beside it, there is a safe.",
-  "On your right, there are stairs leading to a second floor.",
-]
-
-const options_room = [
-  "Go to sink.",
-  "Go to safe.",
-  "Go upstairs.",
-]
 
 const state = {
-  "hasString": false,
-  "hasUnlockedSafe": false,
-  "hasMagnet": false,
-  "combinedMagnetAndString": false,
-  "hasKey": false,
+  "foundString": false,
+  "unlockedSafe": false,
+  "foundMagnet": false,
+  "foundMagnetAndString": false,
+  "foundKey": false,
+}
+
+const rooms = {
+  "main": {
+    "description": [
+      "You are trapped...",
+      "",
+      "On the left, you see an sink.",
+      "Beside it, there is a safe.",
+      "On your right, there are stairs leading to a second floor.",
+    ],
+    "modifies": {},
+    "options": [
+      {
+        "summary": "Go to sink.",
+        "destination": "sink",
+        "requires": {},
+      },
+      {
+        "summary": "Go to safe.",
+        "destination": "main",
+        "requires": {},
+      },
+      {
+        "summary": "Go upstairs.",
+        "destination": "main",
+        "requires": {},
+      },
+    ],
+  },
+  "sink": {
+    "description": [
+      "You approach the small sink squeezed in a corner of the room."
+    ],
+    "modifies": {},
+    "options": [
+      {
+        "summary": "Look inside the sink.",
+        "destination": "sink_hole",
+        "requires": {},
+      },
+      {
+        "summary": "Look under the sink.",
+        "destination": "sink_hole",
+        "requires": {},
+      },
+      {
+        "summary": "Back.",
+        "destination": "main",
+        "requires": {},
+      },
+    ],
+  },
+  "sink_hole": {
+    "description": [
+      "Water drips from the sink tap like a clock,",
+      "chiming as the droplets spatter on the metallic surface."
+    ],
+    "modifies": {},
+    "options": [
+      {
+        "summary": "Inspect sink hole.",
+        "destination": "sink_hole_plugged",
+        "requires": {
+          "foundKey": false,
+        },
+      },
+      {
+        "summary": "Unplug sink hole.",
+        "destination": "sink_hole_unplugged",
+        "requires": {
+          "foundKey": false,
+          "foundMagnetAndString": true,
+        },
+      },
+      {
+        "summary": "Back.",
+        "destination": "sink",
+        "requires": {},
+      },
+    ],
+  },
+  "sink_hole_plugged": {
+    "description": [
+      "There seems to be something shiny peeking out of the dark hole and out of your reach."
+    ],
+    "modifies": {},
+    "options": [
+      {
+        "summary": "Continue.",
+        "destination": "sink_hole",
+        "requires": {},
+      },
+    ],
+  },
+  "sink_hole_unplugged": {
+    "description": [
+      "You retrieve a tiny key from the sink hole.",
+    ],
+    "modifies": {
+      "foundKey": true,
+    },
+    "options": [
+      {
+        "summary": "Continue.",
+        "destination": "sink_hole",
+        "requires": {},
+      },
+    ],
+  },
+}
+
+function hasRequirements(state, requires) {
+  for (const [req, value] of Object.entries(requires)) {
+    if (!state.hasOwnProperty(req) || state[req] !== value) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function getValidOptions(state, options) {
+  let validOptions = [];
+  for (const option of options) {
+    if (!hasRequirements(state, option.requires)) {
+      continue;
+    }
+    validOptions.push(option);
+  }
+  return validOptions;
 }
 
 function promptContinue() {
-  return ask(formatPrompt(prompt_continue));
+  return ask(formatWithPadding(prompt_continue));
 }
 
-async function promptChooseOption(options) {
-  let fullPrompt = prompt_chooseOption;
-
-  options.forEach((option, i) => {
-    fullPrompt += "\n";
-    fullPrompt += formatOption(option, i + 1);
-  });
-
-  return ask(formatPrompt(fullPrompt));
+function promptChooseOption(state, options) {
+  return ask(formatWithPadding(formatAsMenu(options)))
+    .then((optionIndex) => {
+      return options[optionIndex - 1];
+    });
 }
 
 async function setup() {
@@ -82,13 +213,21 @@ async function teardown() {
 }
 
 async function play() {
-  clearLog();
-  displayLines(desc_room);
-  let option = await promptChooseOption(options_room);
+  let currentState = deepcopy(state);
+  let currentRoom = rooms["main"];
 
-  clearLog();
-  displayLine(`You have chosen to ${options_room[option - 1]}!`);
-  await promptContinue();
+  while (true) {
+    clearLog();
+
+    displayDebug(currentState, currentRoom);
+    displayLines(currentRoom.description);
+
+    const validOptions = getValidOptions(currentState, currentRoom.options);
+    let option = await promptChooseOption(currentState, validOptions);
+
+    currentRoom = rooms[option.destination];
+    Object.assign(currentState, currentRoom.modifies);
+  }
 }
 
 async function run() {
